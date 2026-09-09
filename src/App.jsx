0a3cb154b1d966d72fc4b2e7cac59759a9547521
Nathaniel O. M.  Ansah <186203419+Ohene-Nat@users.noreply.github.com>
2026-09-09 06:11:32 +0000

    handling change

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import "./App.css";
 import { useState } from "react";
 
-function Greeting(props) {
-  return (
-    <h1>Hello, I am {props.name}!</h1>
-  );
-}
 function App() {
-  const [showgreeting, setShowGreeting] = useState(false);
-  function toggleGreeting() {
-    if (showgreeting) {
-      setShowGreeting(false);
-    } else {
-      setShowGreeting(true);
-    }
-    console.log(showgreeting);
+  const [name, setName] = useState("");
+  
+  function handleChange(event) {
+    const value = event.target.value;
+    setName(value);
   }
   return (
-    <div >
-      <button onClick={toggleGreeting}>Toggle greeting</button>
-      {showgreeting && <Greeting name="Alice" />}
-    </div>
-  );
+  <div>
+    <input type= "text" placeholder="Name..." onChange={handleChange}/>
+    {name}
+  </div>
+  )
 }
 
 export default App;
