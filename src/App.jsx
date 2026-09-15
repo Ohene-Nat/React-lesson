21a0024daf2d9cfeebd717a44725f79d141bf881
Nathaniel O. M.  Ansah <186203419+Ohene-Nat@users.noreply.github.com>
2026-09-15 16:44:26 +0000

    adding pages to test the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import SignUpForm from "./component/SignUpForm.jsx";
+// import SignUpForm from "./component/SignUpForm.jsx";
 import { Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-// import { useState } from "react";
+import { useState } from "react";
 
 // function App() {
 //   const [name, setName] = useState("");
@@ -45,13 +45,59 @@ import { Link } from "react-router-dom";
 //     </div>
 //   );
 // }
+function Navbar(){
+  return(
+    <header
+    style={{
+      padding: "1rem 1.5rem",
+      marginBottom: "1rem",
+      borderBottom: "1px solid #e5e7eb",
+      display: "flex",
+      justifyContent:"space-between",
+    }}
+    >
+     <nav style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
+        <Link to ="/">Home</Link>
+        <Link to ="/profile">Profile</Link>
+      </nav>
+
+      <div>
+        <Link to="/login">Login</Link>
+      </div>
+    </header>
+  );
+}
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our React application.</p>
+      <h1>Home</h1>
+
+      <p>You are not logged in. Go to the login page to sign in</p>
     </div>
   );
+}
+function LoginPage(){
+  const [name, setName] = useState("");
+  const [user, setUser] = useState({name: "", isAuth: false})
+
+  function handleSubmit(e){
+    e.preventDefault();
+    if (!name.trim()) return;
+    alert("Login");
+  }
+  return(
+    <div style={{ padding: "0 1.5rem"}}>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} style={{marginTop: "1rem"}}>
+        <label htmlFor="">
+          Name
+          <input type="text" placeholder="Type any Name..." value ={name} onChange={(e) => setName(e.target.value)}style={{marginLeft:"0.5rem"}}
+          />
+        </label>
+      </form>
+
+    </div>
+  )
 }
 function AboutPage() {
   return (
@@ -64,14 +110,13 @@ function AboutPage() {
 function App() {
   return (
     <div>
-      <nav style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
-        <Link to ="/">Home</Link>
-        <Link to ="/about">About</Link>
-      </nav>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
       </Routes>
     </div>
   );
